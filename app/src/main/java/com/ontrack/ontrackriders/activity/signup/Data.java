c0eb0cbfb7514168bb8ec6b115f2077cd63454b7
Mohit <mohit95.sikri@gmail.com>
2019-05-09 15:34:01 +0530

    implementing login functionality

---
+++ app/src/main/java/com/ontrack/ontrackriders/activity/signup/Data.java
@@ -0,0 +1,54 @@
+
+package com.ontrack.ontrackriders.activity.signup;
+
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+public class Data {
+
+    @SerializedName("name")
+    @Expose
+    private String name;
+    @SerializedName("email")
+    @Expose
+    private String email;
+    @SerializedName("token")
+    @Expose
+    private String token;
+    @SerializedName("refreshtoken")
+    @Expose
+    private String refreshtoken;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getToken() {
+        return token;
+    }
+
+    public void setToken(String token) {
+        this.token = token;
+    }
+
+    public String getRefreshtoken() {
+        return refreshtoken;
+    }
+
+    public void setRefreshtoken(String refreshtoken) {
+        this.refreshtoken = refreshtoken;
+    }
+
+}
